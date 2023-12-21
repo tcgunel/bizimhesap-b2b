4d6f58c71de8b3a4f0f0b877976cbbfff67b0bf8
Tolga Can Günel <tolga@ctrlf5.com.tr>
2023-12-21 10:45:18 +0300

    gönderilen tarih bilgilerinde timezome olmamalı (dökümantasyonda olmasına rağmen)

--- src/Models/Requests/InvoiceDates.php
+++ src/Models/Requests/InvoiceDates.php
@@ -21,7 +21,7 @@ class InvoiceDates
      */
     public function setInvoiceDate(Carbon $invoiceDate): InvoiceDates
     {
-        $this->invoiceDate = $invoiceDate->format('Y-m-d\TH:i:s.uP');
+        $this->invoiceDate = $invoiceDate->format('Y-m-d\TH:i:s.u');
 
         return $this;
     }
@@ -32,7 +32,7 @@ class InvoiceDates
      */
     public function setDueDate(Carbon $dueDate): InvoiceDates
     {
-        $this->dueDate = $dueDate->format('Y-m-d\TH:i:s.uP');
+        $this->dueDate = $dueDate->format('Y-m-d\TH:i:s.u');
 
         return $this;
     }
@@ -43,7 +43,7 @@ class InvoiceDates
      */
     public function setDeliveryDate(Carbon $deliveryDate): InvoiceDates
     {
-        $this->deliveryDate = $deliveryDate->format('Y-m-d\TH:i:s.uP');
+        $this->deliveryDate = $deliveryDate->format('Y-m-d\TH:i:s.u');
 
         return $this;
     }
